e7b81af52e18b6125b58917bcc69b69c990bd2db
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2024-01-21 21:56:48 -0300

    use JWT_SEED env

--- src/config/jwt.adapter.ts
+++ src/config/jwt.adapter.ts
@@ -1,11 +1,15 @@
 import jwt from 'jsonwebtoken'
+import { envs } from './envs'
+
+//dependencia oculta
+const JWT_SEED = envs.JWT_SEED
 
 export class JwtAdapter {
 
   static async generateToken(payload: any, duration = '2h') {
 
     return new Promise((resolve) => {
-      jwt.sign(payload, 'SEED', { expiresIn: duration }, (error, token) => {
+      jwt.sign(payload, JWT_SEED, { expiresIn: duration }, (error, token) => {
         if (error) {
           resolve(null)
         }
