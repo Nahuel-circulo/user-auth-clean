f57834b1440a4f645b64619d41f9e61f7949063e
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2024-01-25 10:15:20 -0300

    use generics type

--- src/config/jwt.adapter.ts
+++ src/config/jwt.adapter.ts
@@ -18,14 +18,14 @@ export class JwtAdapter {
     })
   }
 
-  static validateToken(token: string) {
+  static validateToken<T>(token: string): Promise<T | null> {
     return new Promise((resolve) => {
 
       jwt.verify(token, JWT_SEED, (error, decoded) => {
         if (error) {
           resolve(null)
         }
-        return resolve(decoded)
+        return resolve(decoded as T)
       })
 
 
