60154efd95c6a24082153465767c49d553e94a65
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2024-01-30 12:45:12 -0300

    user product service and product controller

--- src/presentation/products/routes.ts
+++ src/presentation/products/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { ProductController } from './controller';
 import { AuthMiddleware } from '../middlewares/auth.middleware';
+import { ProductService } from '../services';
 
 
 
@@ -11,8 +12,8 @@ export class ProductRoutes {
 
   static get routes(): Router {
     const router = Router();
-    // const service = new ProductService();
-    const controller = new ProductController();
+    const service = new ProductService();
+    const controller = new ProductController(service);
     router.get('/', controller.GetProducts);
     router.post('/', [AuthMiddleware.validateJWT], controller.createProduct);
     return router;
