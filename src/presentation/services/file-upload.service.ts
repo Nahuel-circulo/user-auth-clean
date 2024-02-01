c9e0ac9721738abb68db755456803cbde93f6935
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2024-02-01 11:14:17 -0300

    implement file-upload service

--- src/presentation/services/file-upload.service.ts
+++ src/presentation/services/file-upload.service.ts
@@ -1,21 +1,51 @@
+import { UploadedFile } from "express-fileupload";
+import path from 'path'
+import fs from 'fs'
+import { Uuid } from "../../config";
+import { CustomError } from "../../domain";
+
 
 
 export class FileUploadService {
 
-  constructor() { }
-
-  private checkFolder(folderPath: string) {
-    throw new Error("Method not implemented.");
-  }
-
-  public uploadSingleFile(
-    file: any,
-    folder: string = 'uploads',
-    validExtensions: string[] = ['png', 'jpg', 'jpeg', 'gif', 'webp']
+  constructor(
+    private readonly uuid = Uuid.v4
   ) { }
 
+  private checkFolder(folderPath: string) {
+    if (!fs.existsSync(folderPath)) {
+      fs.mkdirSync(folderPath)
+    }
+  }
+
+  public async uploadSingleFile(
+    file: UploadedFile,
+    folder: string = 'uploads',
+    validExtensions: string[] = ['png', 'jpg', 'jpeg', 'gif', 'webp']
+  ) {
+
+    try {
+      const fileExtension = file.mimetype.split('/').at(1) ?? '';
+
+      if (!validExtensions.includes(fileExtension)) {
+        throw CustomError.badRequest(`Invalid extension: ${fileExtension}, valid ones ${validExtensions}`);
+      }
+
+      const destination = path.resolve(__dirname, '../../../', folder);
+      this.checkFolder(destination);
+
+      const fileName = `${this.uuid()}.${fileExtension}`;
+      file.mv(destination + `/${fileName}`);
+
+      return fileName;
+
+    } catch (error) {
+      throw error;
+    }
+  }
+
   public uploadMultipleFile(
-    file: any[],
+    file: UploadedFile[],
     folder: string = 'uploads',
     validExtensions: string[] = ['png', 'jpg', 'jpeg', 'gif', 'webp']
   ) { }
